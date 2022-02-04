88db7a4b53942af181955b9861ada264590f8510
연훈 배 <sbaru1997@naver.com>
2022-02-04 23:55:22 +0900

    fix: useEffect 종속성 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,41 @@
-import classNames from 'classnames';
-import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import '../styles/components/Header.scss';
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/components/Header.scss";
 
 const Header = () => {
   const location = useLocation();
   const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
-    if (location.pathname !== '/') {
+    if (location.pathname !== "/") {
       setIsDark(true);
     }
-  }, []);
+  }, [location.pathname]);
 
   return (
-    <div className={classNames('header__navigation')}>
-      <Link to="/" className={classNames('header__navigation--logo', { isDark: isDark })}>
+    <div className={classNames("header__navigation")}>
+      <Link
+        to="/"
+        className={classNames("header__navigation--logo", { isDark: isDark })}
+      >
         헬프애니멍
       </Link>
-      <div className={classNames('header__navigation--menu')}>
-        <Link to="/test" className={classNames('header__navigation--menuLink', { isDark: isDark })}>
+      <div className={classNames("header__navigation--menu")}>
+        <Link
+          to="/test"
+          className={classNames("header__navigation--menuLink", {
+            isDark: isDark,
+          })}
+        >
           test
         </Link>
-        <Link to="/test" className={classNames('header__navigation--menuLink', { isDark: isDark })}>
+        <Link
+          to="/test"
+          className={classNames("header__navigation--menuLink", {
+            isDark: isDark,
+          })}
+        >
           sub
         </Link>
       </div>
