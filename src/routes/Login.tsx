bff26ac237b4baeddb6d153bbe9d41b31b157c62
연훈 배 <sbaru1997@naver.com>
2022-02-16 21:24:54 +0900

    feat: Login 화면 구현
    임시 스타일 적용

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onEmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <>
-      <span>Login</span>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "90vh",
+        }}
+      >
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={onSubmitHandler}
+        >
+          <label>Email</label>
+          <input type="email" value={Email} onChange={onEmailHandler} />
+          <label>Password</label>
+          <input
+            type="password"
+            value={Password}
+            onChange={onPasswordHandler}
+          />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 };
