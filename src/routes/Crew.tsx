29863e0f773c7f8dc181db07a08774601d147406
연훈 배 <sbaru1997@naver.com>
2022-02-19 23:58:25 +0900

    feat: 내 크루 표시 영역 구현

--- src/routes/Crew.tsx
+++ src/routes/Crew.tsx
@@ -1,7 +1,22 @@
+import MyCrewBoard from "../components/MyCrewCard";
+import "../styles/routes/crew.scss";
+import { myCrew } from "../test/data/crew";
+
 const Crew = () => {
+  const myCrewArray = myCrew();
   return (
     <>
-      <span>Crew</span>
+      <section className="my-crew__section">
+        {myCrewArray.map((board) => (
+          <MyCrewBoard
+            key={board.index}
+            image={board.image}
+            name={board.name}
+            amount={board.amount}
+            region={board.region}
+          />
+        ))}
+      </section>
     </>
   );
 };
