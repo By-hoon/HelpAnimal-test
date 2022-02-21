23a98278abbd0528e12c4dbc7beb32859aff3767
연훈 배 <sbaru1997@naver.com>
2022-02-21 22:00:27 +0900

    feat: 크루 리스트 출력 섹션 구현

--- src/routes/Crew.tsx
+++ src/routes/Crew.tsx
@@ -1,21 +1,28 @@
-import MyCrewBoard from "../components/MyCrewCard";
+import CrewCard from "../components/CrewCard";
 import "../styles/routes/crew.scss";
-import { myCrew } from "../test/data/crew";
+import { CREWS } from "../test/data/crew";
 
 const Crew = () => {
-  const myCrewArray = myCrew();
+  const crews = CREWS.WHOLE;
   return (
     <>
-      <section className="my-crew__section">
-        {myCrewArray.map((board) => (
-          <MyCrewBoard
-            key={board.index}
-            image={board.image}
-            name={board.name}
-            amount={board.amount}
-            region={board.region}
-          />
-        ))}
+      <section className="crew-list__section">
+        {/* <div className="section-bar__container">
+          <span className="section-name__span">크루</span>
+        </div> */}
+        <div className="crew-list__container">
+          {crews.map((crew) => (
+            <CrewCard
+              key={crew.id}
+              id={crew.id}
+              image={crew.image}
+              name={crew.name}
+              introduction={crew.introduction}
+              amount={crew.amount}
+              region={crew.region}
+            />
+          ))}
+        </div>
       </section>
     </>
   );
