68d378be4c3dca62dea20eca9c8481e581487da3
연훈 배 <sbaru1997@naver.com>
2022-02-24 21:25:11 +0900

    feat: 크루 생성, 관리 페이지 라우팅 및 없는 URL 이동시 리다이렉팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Header from "./components/Header";
 import Test from "./routes/Test";
 import Login from "./routes/Login";
 import Crew from "./routes/crew/Crew";
+import CrewCreate from "./routes/crew/Create";
+import CrewManagement from "./routes/crew/Management";
 
 const Router = () => {
   return (
@@ -14,6 +16,9 @@ const Router = () => {
         <Route path="/test" element={<Test />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/crew" element={<Crew />}></Route>
+        <Route path="/crew/create" element={<CrewCreate />}></Route>
+        <Route path="/crew/management/:id" element={<CrewManagement />}></Route>
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
   );
